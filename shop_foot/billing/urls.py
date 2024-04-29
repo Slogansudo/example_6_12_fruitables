39724385d60363ba1258b42fd1b63712c7161bc8
Slogan <srmslogan6040@gmail.com>
2024-04-29 16:08:20 +0500

    Delete shop_foot directory

--- shop_foot/billing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CheckOutView, TestimonialView
-
-urlpatterns = [
-    path('checkout/', CheckOutView.as_view(), name='checkout'),
-    path('testemonial/', TestimonialView.as_view(), name="testimonial")
-]
-
--- shop_foot/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProductListView, ProductDetailView, CartView
-
-urlpatterns = [
-    path('shop/', ProductListView.as_view(), name='shopping'),
-    path('shop/detail/<int:id>/', ProductDetailView.as_view(), name='shop-detail'),
-    path('cart/<int:id>/detail', CartView.as_view(), name='cart')
-]
